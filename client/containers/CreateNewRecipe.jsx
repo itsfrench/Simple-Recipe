37b1d116f5c60a7a3b72d135848f88cb058d48e3
Cheri French <clynnfrench@gmail.com>
2023-11-08 13:31:09 -0500

    Built and styled form.

--- client/containers/CreateNewRecipe.jsx
+++ client/containers/CreateNewRecipe.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 const RecipeCreator = () => {
 
   return (
-    <div>
-      <h3>Create a new recipe</h3>
-      <div>
-        <label>Title:</label>
-        <input id='form' type='text' name='market-namer' ></input>  {/* onChange={setLocation} */}
-        <button id={'locationButton'}>Add Market</button>
+    <div id='create'>
+      <div id='recipe-form'>
+        <label>Recipe Title:</label>
+        <input id='form-title' type='text' name='recipe-title' ></input>
+        <label>Measurment Type:</label>
+        <input type='datalist' list='measurment' />
+          <datalist id='measurment'>
+            <option value="Metric">Metric</option>
+            <option value="US Standard">US Standard</option>
+          </datalist>  
+        <label> Recipe Ingredients:</label>
+        <input id='form-ingredients' type='text' name='recipe-title' ></input>
+        <label>Recipe Instructions:</label>
+        <input id='form-instructions' type='text' name='recipe-title' ></input> 
+        <button id='create-button'>Add Recipe</button>
         </div>
     </div>
   )
