14e1a492060538af28ee52a3f55794df00792b4b
Cheri French <clynnfrench@gmail.com>
2023-11-08 20:10:05 -0500

    Adding a new file to my repo.

---
+++ client/containers/RecipeCards.jsx
@@ -0,0 +1,28 @@
+// import React from 'react';
+//import { useState } from 'react';
+// import { useSelector } from 'react-redux';
+
+// const RecipeCards = () => {
+//   // const allRecipes = useSelector(state => state.recipeList);
+//   // const recipes = [];
+  
+//   // for (const obj in allRecipes) {
+//   //   recipes.push(
+//   //     <div id={allRecipes[obj].id}>
+//   //       <h3>Recipe Title: </h3>
+//   //       <p>{allRecipes[obj].title}</p>
+//   //         <h3>Ingredients: </h3>
+//   //         <p>{allRecipes[obj].ingredients}</p>
+//   //         <h3>Instructions: </h3>
+//   //         <p>{allRecipes[obj].instructions}</p>
+//   //     </div>
+//   //   )
+//   // }
+//   //{recipes}
+
+//   return (
+//     <div></div>
+//   )
+// }
+
+// export default RecipeCards;
